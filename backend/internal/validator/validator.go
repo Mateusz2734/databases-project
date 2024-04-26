01b9327d4aad4dddee057a35e92307fc6c3c25a9
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-04-26 15:12:55 +0200

    fix: Do not hold status in Validator

--- backend/internal/validator/validator.go
+++ backend/internal/validator/validator.go
@@ -1,7 +1,6 @@
 package validator
 
 type Validator struct {
-	Status      string            `json:"status"`
 	Errors      []string          `json:"errors,omitempty"`
 	FieldErrors map[string]string `json:"fieldErrors,omitempty"`
 }
