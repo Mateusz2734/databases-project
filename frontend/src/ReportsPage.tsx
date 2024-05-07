adf052c6302a026e5d464c1db0b81a1cc7f88336
Mateusz Wala <mateusz.wala2734@gmail.com>
2024-05-07 10:46:38 +0200

    core: Make Chart a separate component

--- frontend/src/ReportsPage.tsx
+++ frontend/src/ReportsPage.tsx
@@ -1,114 +1,55 @@
 import { useEffect, useState } from 'react';
-import Highcharts, { Options } from 'highcharts/highstock';
-import HighchartsReact from 'highcharts-react-official';
 import axios from 'axios';
-import moment from 'moment';
+import { Chart } from './components/Chart';
 
-Highcharts.setOptions({
-    lang: {
-        rangeSelectorZoom: 'Granularity'
-    },
-    rangeSelector: {
-        inputEnabled: false,
-        allButtonsEnabled: true,
-        buttonTheme: {
-            width: 60
-        }
-    },
-    scrollbar: {
-        enabled: false,
-    },
-});
+type SeatTypeEarnings = {
+    seat_type: string;
+    value: number;
+};
+
+type DestinationPopularity = {
+    arrival_airport: string;
+    seat_count: number;
+};
+
+type RoutePopularity = {
+    departure_airport: string;
+    arrival_airport: string;
+    seat_count: number;
+};
+
+type ReportsResponse = {
+    periodic_earnings: number[][],
+    total_earnings: SeatTypeEarnings[],
+    tickets_sold: number,
+    popular_destinations: DestinationPopularity[],
+    popular_flights: RoutePopularity[];
+};
 
 export default function ReportsPage() {
-    const [seriesData, setSeriesData] = useState<number[][] | []>([]);
-    const [type, setType] = useState<'line' | 'bar'>('line');
-    const setLine = () => setType('line');
-    const setBar = () => setType('bar');
+    const [chartData, setChartData] = useState<number[][] | []>([]);
 
     useEffect(() => {
         const loadData = async () => {
             try {
-                const resp = await axios.get<number[][]>('http://localhost:4444/reports');
-                setSeriesData(resp.data);
+                const resp = await axios.get<ReportsResponse>('http://localhost:4444/reports?from=2024-01-01&to=2024-12-31&limit=20');
+                console.log(resp.data);
+                setChartData(resp.data.periodic_earnings);
             } catch (error) {
                 console.log(error);
             }
         };
 
-        if (seriesData.length === 0) {
+        if (chartData.length === 0) {
             loadData();
         }
     }, []);
 
-    const options: Options = {
-        title: {
-            text: 'Income from flights'
-        },
-        xAxis: {
-            plotLines: [{
-                color: 'black',
-                dashStyle: 'Dash',
-                value: type === 'line' ? new Date().getTime() : 0,
-                width: 1,
-                zIndex: 2,
-                label: {
-                    text: 'Current Day',
-                    align: 'center',
-                    y: 50
-                }
-            }]
-        },
-        navigator: {
-            enabled: false,
-        },
-        tooltip: {
-            formatter: function () {
-                return [type === 'line' ? moment(this.x).format("YYYY-MM-DD") : ""].concat(
-                    this.points ?
-                        this.points.map(function (point) {
-                            return point.y?.toLocaleString(undefined, { maximumFractionDigits: 2 }) + ' PLN';
-                        }) : []
-                );
-            },
-        },
-        series: [{ type: type, data: seriesData }],
-        rangeSelector: {
-            buttons: [{
-                text: 'Day',
-                events: { click: setLine },
-                dataGrouping: {
-                    units: [['day', [1]]],
-                    forced: true,
-                    approximation: 'sum'
-                },
-            }, {
-                text: 'Week',
-                events: { click: setLine },
-                dataGrouping: {
-                    units: [['week', [1]]],
-                    forced: true,
-                    approximation: 'sum'
-                },
-            }, {
-                text: 'Month',
-                events: { click: setBar },
-                dataGrouping: {
-                    units: [['month', [1]]],
-                    forced: true,
-                    approximation: 'sum'
-                }
-            }]
-        },
-    };
-
     return (
-        <div>
-            <HighchartsReact
-                highcharts={Highcharts}
-                constructorType='stockChart'
-                options={options}
-            />
+        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+            <div style={{ width: "80%" }}>
+                <Chart chartData={chartData} />
+            </div>
         </div>
     );
 }
---
+++ frontend/src/components/Chart.tsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import Highcharts, { Options } from 'highcharts/highstock';
+import HighchartsReact from 'highcharts-react-official';
+import moment from 'moment';
+
+Highcharts.setOptions({
+    lang: {
+        rangeSelectorZoom: 'Granularity'
+    },
+    rangeSelector: {
+        inputEnabled: false,
+        allButtonsEnabled: true,
+        buttonTheme: {
+            width: 60
+        }
+    },
+    scrollbar: {
+        enabled: false,
+    },
+});
+
+export function Chart(props: { chartData: number[][]; }) {
+    const [type, setType] = useState<'line' | 'bar'>('line');
+    const setLine = () => setType('line');
+    const setBar = () => setType('bar');
+
+    const options: Options = {
+        title: {
+            text: 'Income from flights'
+        },
+        xAxis: {
+            plotLines: [{
+                color: 'black',
+                dashStyle: 'Dash',
+                value: type === 'line' ? new Date().getTime() : 0,
+                width: 1,
+                zIndex: 2,
+                label: {
+                    text: 'Current Day',
+                    align: 'center',
+                    y: 50
+                }
+            }]
+        },
+        navigator: {
+            enabled: false,
+        },
+        tooltip: {
+            formatter: function () {
+                return [type === 'line' ? moment(this.x).format("YYYY-MM-DD") : ""].concat(
+                    this.points ?
+                        this.points.map(function (point) {
+                            return point.y?.toLocaleString(undefined, { maximumFractionDigits: 2 }) + ' PLN';
+                        }) : []
+                );
+            },
+        },
+        series: [{ type: type, data: props.chartData }],
+        rangeSelector: {
+            buttons: [{
+                text: 'Day',
+                events: { click: setLine },
+                dataGrouping: {
+                    units: [['day', [1]]],
+                    forced: true,
+                    approximation: 'sum'
+                },
+            }, {
+                text: 'Week',
+                events: { click: setLine },
+                dataGrouping: {
+                    units: [['week', [1]]],
+                    forced: true,
+                    approximation: 'sum'
+                },
+            }, {
+                text: 'Month',
+                events: { click: setBar },
+                dataGrouping: {
+                    units: [['month', [1]]],
+                    forced: true,
+                    approximation: 'sum'
+                }
+            }]
+        },
+    };
+
+    return (
+        <HighchartsReact
+            highcharts={Highcharts}
+            constructorType='stockChart'
+            options={options}
+        />
+    );
+}
